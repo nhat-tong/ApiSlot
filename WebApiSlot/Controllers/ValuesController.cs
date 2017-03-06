8c7e50ef64d48af270e7cf4c756c25efe961addc
Hoang Quoc Nhat TONG <htong@expertime.com>
2017-03-06 14:07:06 +0100

    update api

--- WebApiSlot/Controllers/ValuesController.cs
+++ WebApiSlot/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace WebApiSlot.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", "value3", "value4", "value5", "value6" };
+            return new string[] { "value1", "value2", "value3", "value4", "value5", "value6", "value7", "value8" };
         }
 
         // GET api/values/5
